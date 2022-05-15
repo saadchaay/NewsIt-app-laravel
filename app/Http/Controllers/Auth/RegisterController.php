dc33226626b145f1a8fefeccb4a1b92b7025f7a1
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-05-15 11:02:36 +0000

    add form register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -8,7 +8,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 
 
-class RegisterController extends Controller
+class RegisterController extends Controller 
 {
     public function __construct()
     {
@@ -29,15 +29,15 @@ class RegisterController extends Controller
             'password' => 'required|confirmed',
         ]);
 
-        User::create([
-            'name' => $request->name,
-            'username' => $request->username,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        // User::create([
+        //     'name' => $request->name,
+        //     'username' => $request->username,
+        //     'email' => $request->email,
+        //     'password' => Hash::make($request->password),
+        // ]);
 
-        auth()->attempt($request->only('email', 'password'));
+        // auth()->attempt($request->only('email', 'password'));
 
-        return redirect()->route('dashboard');
+        // return redirect()->route('dashboard');
     }
 }
